6321e906d6b167e7bfd5c65e8a35f3d37557b59a
ldermign <ldermign@student.42.fr>
2021-03-22 14:47:36 +0100

    t

--- Dockerfile
+++ Dockerfile
@@ -6,23 +6,25 @@ RUN apt-get upgrade -y
 RUN apt-get -y install wget
 RUN apt-get -y install nginx
 RUN apt-get install -y mariadb-server
+# certificat ssl, deja installe ?
+# RUN apt-get -y install openssl
 #installation php
 RUN apt-get -y install php7.3 php7.3-fpm php7.3-mysql php-common php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline
 
 # localisation des recherches server-web, obligation unzip, dou wget
 # installation phpMyAdmin
-WORKDIR /var/www/html/
+WORKDIR /var/www/html/phpmyadmin
 RUN wget https://files.phpmyadmin.net/phpMyAdmin/5.0.1/phpMyAdmin-5.0.1-english.tar.gz
 RUN tar -xf phpMyAdmin-5.0.1-english.tar.gz && rm -rf phpMyAdmin-5.0.1-english.tar.gz
 RUN mv phpMyAdmin-5.0.1-english phpmyadmin
 
 # installer wordpress
-WORKDIR /var/www/html/
+WORKDIR /var/www/html
 RUN wget https://wordpress.org/latest.tar.gz
 RUN tar -xvzf latest.tar.gz && rm -rf latest.tar.gz
 
-COPY ./srcs/ /tmp
+WORKDIR /tmp/
 
-# se mettre dans fichier /tmp car on a tout copie dedans, dans le conteneur, precedemment
-WORKDIR /tmp
+COPY ./srcs/ .
+
 CMD bash init.sh
--- srcs/init.sh
+++ srcs/init.sh
@@ -6,29 +6,28 @@ rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 cp /tmp/default /etc/nginx/sites-available
 ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
 
-cp ./srcs/wp-config.php /var/www/html
-
 # Configure a wordpress database
-echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
-echo "GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost' WITH GRANT OPTION;"| mysql -u root --skip-password
-echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
-#echo "update mysql.user set plugin='' where user='root';"| mysql -u root --skip-password
+echo "CREATE DATABASE wordpress;"| mysql -u root
+echo "GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;"| mysql -u root
+echo "FLUSH PRIVILEGES;"| mysql -u root
 
+cp wp-config.php /var/www/html/
+cp config.inc.php /var/www/html/phpmyadmin
 
-## A VOIR
-# mkdir /var/www/localhost
+cd /etc/
+mkdir .mkcert
+chmod +x mkcert
+apt-get -y install libnss3-tools
+./mkcert -install
+./mkcert localhost
 
-# creer le fichier a afficher quand index on
-# cp localhost_index_on /etc/nginx/sites-available/localhost
-# ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/
-
-
-#echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root
-#echo "GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'password';" | mysql -u root
-#echo "FLUSH PRIVILEGES;" | mysql -u root
+#cd /etc/ssl/
+#openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/ssl/localhost.pem -keyout /etc/ssl/localhost.key -subj "/C=FR/ST=Paris/L=Paris/O=42/OU=user/CN=localhost"
 
 service nginx reload
 service mysql reload
 service php7.3-fpm reload
+
+cd /tmp/
 
 bash
