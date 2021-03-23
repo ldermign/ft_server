54ba645bf8f422b641cdb9ea53094c30f6ab23fc
ldermign <ldermign@student.42.fr>
2021-03-23 11:40:47 +0100

    avancement

--- Dockerfile
+++ Dockerfile
@@ -2,24 +2,20 @@ FROM debian:buster-slim
 
 RUN apt-get update 
 RUN apt-get upgrade -y
-# get .tar file of phpmyadmin and wordpress
 RUN apt-get -y install wget
 RUN apt-get -y install nginx
 RUN apt-get install -y mariadb-server
-# certificat ssl, deja installe ?
-# RUN apt-get -y install openssl
 #installation php
 RUN apt-get -y install php7.3 php7.3-fpm php7.3-mysql php-common php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline
 
-# localisation des recherches server-web, obligation unzip, dou wget
+WORKDIR /var/www/html/
+
 # installation phpMyAdmin
-WORKDIR /var/www/html/phpmyadmin
 RUN wget https://files.phpmyadmin.net/phpMyAdmin/5.0.1/phpMyAdmin-5.0.1-english.tar.gz
 RUN tar -xf phpMyAdmin-5.0.1-english.tar.gz && rm -rf phpMyAdmin-5.0.1-english.tar.gz
 RUN mv phpMyAdmin-5.0.1-english phpmyadmin
 
 # installer wordpress
-WORKDIR /var/www/html
 RUN wget https://wordpress.org/latest.tar.gz
 RUN tar -xvzf latest.tar.gz && rm -rf latest.tar.gz
 
--- srcs/init.sh
+++ srcs/init.sh
@@ -2,27 +2,34 @@ service nginx start
 service mysql start
 service php7.3-fpm start
 
+rm /var/www/html/index.nginx-debian.html
+cp info.php /var/www/html/
+
 rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-cp /tmp/default /etc/nginx/sites-available
+cp default /etc/nginx/sites-available
 ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
 
 # Configure a wordpress database
-echo "CREATE DATABASE wordpress;"| mysql -u root
-echo "GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;"| mysql -u root
-echo "FLUSH PRIVILEGES;"| mysql -u root
+echo "CREATE DATABASE wordpress;" | mysql -u root
+echo "CREATE USER 'utilisateur'@'%';" | mysql -u root
+echo "GRANT ALL ON wordpress.* TO 'utilisateur'@'%' WITH GRANT OPTION;" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
 
-cp wp-config.php /var/www/html/
+cp wp-config.php /var/www/html/wordpress
 cp config.inc.php /var/www/html/phpmyadmin
 
+chown -R www-data:www-data /var/www/html/*
+chmod 755 -R /var/www/*
+
+# configuration certificat ssl
 cd /etc/
 mkdir .mkcert
+cd ./.mkcert
+wget -O mkcert https://github.com/FiloSottile/mkcert/releases/download/v1.2.0/mkcert-v1.2.0-linux-amd64
 chmod +x mkcert
 apt-get -y install libnss3-tools
 ./mkcert -install
 ./mkcert localhost
-
-#cd /etc/ssl/
-#openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/ssl/localhost.pem -keyout /etc/ssl/localhost.key -subj "/C=FR/ST=Paris/L=Paris/O=42/OU=user/CN=localhost"
 
 service nginx reload
 service mysql reload
